41fb62b159f6222c6de68db6821999ce820f9be7
Anderson D. Rosa <andersondrosa@outlook.com>
2022-02-14 20:45:51 -0300

    ..

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FunctionComponent, ReactNode } from 'react';
 import mergeDeepRight from './mergeDeepRight';
 
 const context = React.createContext({});
@@ -8,12 +8,46 @@ const useContext = () => {
   return { data };
 };
 
-const Context = ({ children, data, merge }: any) => {
+export type ContextProps = { children?: ReactNode; data: object };
+
+const Context = ({ data, children }: ContextProps) => {
+  return <context.Provider value={data}>{children}</context.Provider>;
+};
+
+export type ContextLayerProps = {
+  children?: ReactNode;
+  map?: Function;
+  data?: object;
+  merge?: boolean;
+};
+
+Context.layer = ({ children, data, merge, map }: ContextLayerProps) => {
   const rootData = React.useContext(context);
   const value = !merge
     ? Object.assign({}, rootData, data)
     : mergeDeepRight(rootData, data);
-  return React.createElement(context.Provider, { value }, children);
+  return (
+    <context.Provider value={map ? map(value) : value}>
+      {children}
+    </context.Provider>
+  );
 };
 
-export { Context, useContext };
+export type ConsumerProps = {
+  children?: any;
+  map?: Function;
+};
+
+const Consumer = ({ children: Child, map }: ConsumerProps) => {
+  return (
+    <context.Consumer>
+      {(values: any) => <Child {...(map ? map(values) : values)} />}
+    </context.Consumer>
+  );
+};
+
+const connect = (component: FunctionComponent, map?: Function) => {
+  return () => <Consumer map={map}>{component}</Consumer>;
+};
+
+export { Context, useContext, Consumer, connect };
--- test/data-layer.test.tsx
+++ test/data-layer.test.tsx
@@ -1,26 +1,66 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Context, useContext } from '../src/index';
+import { Context, Consumer, connect } from '../src/index';
+
+import { assoc, pick, pipe } from 'ramda';
+// import { path } from 'ramda';
+// import { mergeDeepLeft } from 'ramda';
 
 describe('it', () => {
   it('should works', () => {
     const div = document.createElement('div');
 
-    const Json = () => {
-      const { data } = useContext();
-      console.log(data);
-      return <div>{JSON.stringify(data)}</div>;
+    const Log = (props: any) => {
+      console.log(props);
+      return <></>;
     };
 
+    const LogWiFi = connect(Log, pick(['data']));
+
     ReactDOM.render(
-      <Context data={{ data: { name: 'ninja' } }} merge>
-        <Json />
-        <Context data={{ data: { samurai: 'Jack' } }} merge>
-          <Json />
-        </Context>
-        <Json />
+      <Context data={{ data: { name: 'init' } }}>
+        <LogWiFi />
+        <Context.layer data={{ data: { name2: 'Foo' }, last: 'data' }} merge>
+          <LogWiFi />
+          <Consumer map={(x: any) => ({ ...x, other: 'true' })}>{Log}</Consumer>
+          <Context.layer data={{ data: { name: 'Bar' } }} merge>
+            <Context.layer
+              map={pipe(
+                assoc('other-name', 'NAME'),
+                assoc('other-label', 'LABEL')
+              )}
+            >
+              <Consumer map={pick(['other-name', 'last'])}>{Log}</Consumer>
+            </Context.layer>
+          </Context.layer>
+          <LogWiFi />
+        </Context.layer>
+        <LogWiFi />
       </Context>,
       div
     );
   });
 });
+
+// describe('it', () => {
+//   it('should works', () => {
+//     const div = document.createElement('div');
+
+//     const Json = () => {
+//       const { data } = useContext();
+//       console.log(data);
+//       return <div>{JSON.stringify(data)}</div>;
+//     };
+
+//     ReactDOM.render(
+//       <Context data={{ data: { name: 'ninja' } }} merge>
+//         <Json />
+//         <Context data={{ data: { samurai: 'Jack' } }} merge>
+//           <Json />
+//         </Context>
+//         <Json />
+//       </Context>,
+//       div
+//     );
+//   });
+// });
